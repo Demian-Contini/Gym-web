cf5bc1295372bb5b280ce2b9e7b3708f409752dd
Demian Contini <demiandtc@gmail.com>
2025-07-22 20:16:40 -0300

    Corrige catch sin parámetro para evitar error

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -48,7 +48,7 @@ export default function Form() {
       } else {
         setError('Hubo un error al enviar el mensaje. Intenta nuevamente.');
       }
-    } catch (_) {
+    } catch {
       setError('Hubo un error al enviar el mensaje. Intenta nuevamente.');
     }
   };
